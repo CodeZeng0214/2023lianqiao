1a1d316af9a9f078834b36bdb229a209fbe27937
eys <97833866+CodeZeng0214@users.noreply.github.com>
2023-03-24 20:44:48 +0800

    3.24-trie和KMP

---
+++ DailyAC/3.22-141. 周期/源.cpp
@@ -0,0 +1,134 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N;
+char str[1000010];
+int ne[1000010];
+
+//求出next数组
+void GetNext(char s[])
+{
+	ne[1] = 0;
+	for (int i = 2, j = 0; i <= N; i++) 
+	{
+		while (j && s[i] != s[j + 1]) j = ne[j];
+		if (s[i] == s[j + 1]) j++;
+		ne[i] = j;
+	}
+
+	//自己的求法，求得不对
+	/*while (i < N)
+	{
+		if (j == 0 || s[i] == s[j])
+		{
+			ne[++i] = ++j;
+		}
+		else
+		{
+			j = ne[j];
+		}
+	}*/
+}
+
+
+int main()
+{
+	int n = 1;
+	while (1)
+	{
+		cin >> N;
+		if (!N) break;
+		cin >> str + 1;//可以将字符串从数组下标一开始录入
+		cout << str;
+		GetNext(str);//KMP算法的next数组
+		cout << "Test case #" << n++ << endl;
+		for (int i = 1; i <= N; i++)
+		{
+			int t = i - ne[i];
+			if (i % t == 0 && i / t > 1)
+			{
+				cout << i << " " << i / t << endl;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+////int a[100005];
+//
+//int main()
+//{
+//	int N;
+//	int n = 1;
+//	while(1)
+//	{
+//		cin >> N;
+//		if (!N) break;
+//		cin >> str;
+//		cout << "Test case #" << n++ << endl;
+//		//for (int i = 0; i < N / 2; i++)
+//		//{
+//		//	int res = 0;
+//		//	s = str.substr(0, i + 1);
+//		//	while (++res)
+//		//	{
+//		//		if ((i + 1) * res > N) break;
+//		//		if (s != str.substr((i + 1) * res, i + 1))
+//		//		{
+//		//			a[(i + 1) * res - 1] = i + 1;
+//		//			break;
+//		//		}
+//		//	}
+//		//}
+//		//for (int i = N - 1; i >= 0; i--)
+//		//{
+//		//	if (a[i])//遇到有数的
+//		//	{
+//		//		continue;
+//		//	}
+//		//	else
+//		//	{
+//		//		a[i - 1] += a[i];
+//		//	}
+//		//}
+//		//for (int i = 0; i < N; i++)
+//		//{
+//		//	if (a[i] == 0 || (i + 1) % a[i])//不能整除
+//		//	{
+//		//		continue;
+//		//	}
+//		//	else
+//		//	{
+//		//		cout << i + 1 << " " << a[i] << endl;
+//		//	}
+//		//}
+//
+//		for (int i = 1; i < N; i++)
+//		{
+//			for (int j = 0; j < (i + 1) / 2; j++)
+//			{
+//				if ((i + 1) % (j + 1)) continue;
+//				int res = (i + 1) / (j + 1);
+//				s = str.substr(0, j + 1);
+//				bool same = true;
+//				while (res--)
+//				{
+//					if (s != str.substr((j + 1) * res, j + 1))
+//					{
+//						same = false;
+//						break;
+//					}
+//				}
+//				if (same)
+//				{
+//					cout << i + 1 << " " << (i + 1) / (j + 1) << endl;
+//					break;
+//				}
+//			}
+//		}
+//		cout <<endl;
+//	}
+//	return 0;
+//}
---
+++ DailyAC/3.24-3485. 最大异或和/源.cpp
@@ -0,0 +1,58 @@
+//https://www.bilibili.com/video/BV1yA4y1Z74t/?spm_id_from=333.337.search-card.all.click&vd_source=caecf80374a9146fe3069ef72b4bd1c4
+//trie可以参看上面的视频讲解
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3100010;
+int n, m;
+int tr[N][2], cnt[N], idx;//定义trie的数据结构，tr数组的N代表，2代表该位选0或者选1，每个数组的值代表一个唯一的数字，cnt就是数字出现的次数
+int s[N];//前缀异或和数组
+
+void Insert(int x, int v)//支持删除和增加的trie
+{
+	int p = 0;
+	for (int i = 30; i >= 0; i--)
+	{
+		int u = x >> i & 1;//取出第几位
+		if (!tr[p][u]) tr[p][u] = ++idx;
+		p = tr[p][u];
+		cnt[p] += v;
+	}
+}
+
+int Query(int x)//查询最大值
+{
+	int res = 0, p = 0;
+	for (int i = 30; i >= 0; i--)
+	{
+		int u = x >> i & 1;
+		if (cnt[tr[p][!u]]) p = tr[p][!u], res = res * 2 + 1;//如果能找到异或为1的就转向那个结点，并算一下异或的值
+		else p = tr[p][u], res *= 2;//找不到就相同的结点，算一下    //找不找到的用cnt这个数组，存的是结点值
+	}
+	return res;
+}
+
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		int x;
+		cin >> x;
+		s[i] = s[i - 1] ^ x;
+	}
+
+	int ans = 0;
+	Insert(s[0], 1);//考虑起始，可以为空数组，即00000···，则
+	for (int i = 1; i <= n; i++)
+	{
+		if (i - m - 1 >= 0) Insert(s[i - m - 1], -1);//滑动窗口删去前面的值
+		ans = max(ans, Query(s[i]));
+		Insert(s[i], 1);
+	}
+
+	cout << ans;
+	return 0;
+}
